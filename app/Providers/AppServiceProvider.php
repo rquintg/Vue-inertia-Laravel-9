7aaa3ae79b349856c9769d7fbf5702c0fd9278a8
rquintg <ricardoqg41@icloud.com>
2022-06-17 13:45:05 -0500

    :fire: corrigiendo errores

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        if (env('REDIRECT_HTTPS')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
     }
 
     /**
@@ -26,10 +22,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if (env('REDIRECT_HTTPS')) {
-            $url->formatScheme('https://');
-        }
+        JsonResource::withoutWrapping();
     }
 }
